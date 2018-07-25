42fa2f59296a43609ddc38709a8f3ad5e9f05108
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-07-25 03:29:12 +0100

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "plugins": [
+      "mocha"
+    ],
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 8,
+        "ecmaFeatures": {
+
+        }
+    },
+    "env": {
+        "node": true,
+        "es6": true,
+        "mocha": true
+    },
+    "globals": {
+      "welcomeMessage": true,
+      "wrap": true,
+      "debugger": true
+    },
+    "rules": {
+        "no-console":0,
+        "no-unused-vars": 1,
+        "no-case-declarations": 0,
+        "indent": [
+            "warn"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
