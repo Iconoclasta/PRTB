02f69ec5a408f8edd65eb220165b956760ebfaee
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-08-10 16:16:17 +0100

    Update user.js

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -39,8 +39,8 @@ s.schema.statics.authCertainUser = async function (token, username) {
 
 s.schema.statics.tip = async function (tipper, receiver, amount) {
     return this.validateWithdrawAmount(tipper, amount).then(() => {
-        return this.findOneAndUpdate({ username: tipper.username }, { $inc : {'balance' : Decimal(0).minus(Decimal(amount).div(1e-8)).toFixed() } }).then(() => {
-            return this.findOneAndUpdate({ username: receiver.username }, { $inc : {'balance' : Decimal(amount).div(1e-8).toFixed() } });
+        return this.findOneAndUpdate({ username: tipper.username }, { $inc : {'balance' : Decimal(0).minus(Decimal(amount).div(1e-8)).toFixed(3) } }).then(() => {
+            return this.findOneAndUpdate({ username: receiver.username }, { $inc : {'balance' : Decimal(amount).div(1e-8).toFixed(3) } });
         });
     });
 };
@@ -48,7 +48,7 @@ s.schema.statics.tip = async function (tipper, receiver, amount) {
 s.schema.statics.deposit = async function (user, amount) {
     return new Promise((res, rej) => {
         this.validateDepositAmount(user, amount).then(() => {
-            this.findOneAndUpdate({ _id: user._id }, { $inc : {'balance' : Decimal(amount).div(1e-8).toFixed() } }).then((r) => res(r));
+            this.findOneAndUpdate({ _id: user._id }, { $inc : {'balance' : Decimal(amount).div(1e-8).toFixed(3) } }).then((r) => res(r));
         }).catch((err) => rej(err));
     });
 };
@@ -56,13 +56,13 @@ s.schema.statics.deposit = async function (user, amount) {
 s.schema.statics.withdraw = async function (user, amount) {
     return new Promise((res, rej) => {
         this.validateWithdrawAmount(user, amount).then(() => {
-            this.findOneAndUpdate({ _id: user._id }, { $inc : {'balance' : Decimal(0).minus(Decimal(amount).div(1e-8)).toFixed() } }).then((r) => res(r));
+            this.findOneAndUpdate({ _id: user._id }, { $inc : {'balance' : Decimal(0).minus(Decimal(amount).div(1e-8)).toFixed(3) } }).then((r) => res(r));
         }).catch((err) => rej(err));
     });
 };
 
 s.schema.statics.validateDepositAmount = function (user, amount) {
-    if (amount <= 0) return Promise.reject({ message: "zero or negative amount not allowed" });
+    if (amount <= 0) return Promise.reject({ message: "Zero or negative amount not allowed" });
 
     return Promise.resolve({});
 };
@@ -71,9 +71,9 @@ s.schema.statics.validateWithdrawAmount = async function (user, amount) {
 
     amount = Decimal(amount);
 
-    if (amount.isNaN()) return Promise.reject({ message: "amount is not a number" });
-    else if (amount.lessThan(0.1)) return Promise.reject({ message: "Requires at least 0.1 pivx" });
-    else if (amount.greaterThan(Decimal(user.balance.toString()).mul(1e-8))) return Promise.reject({ message: "insufficient funds" });
+    if (amount.isNaN()) return Promise.reject({ message: "That amount is not a number." });
+    else if (amount.lessThan(0.1)) return Promise.reject({ message: "The minimum amount allowed to withdraw is 0.1 PIVX." });
+    else if (amount.greaterThan(Decimal(user.balance.toString()).mul(1e-8))) return Promise.reject({ message: "You do not have sufficient funds!" });
 
     return Promise.resolve({});
 };
@@ -83,8 +83,8 @@ s.schema.statics.validateTipAmount = async function (user, amount) {
     amount = Decimal(amount);
 
     if (amount.isNaN()) return Promise.reject({ message: "That amount is not a number." });
-    else if (amount.lessThan(0.0001)) return Promise.reject({ message: "The minimum amount allowed to tip is 0.0001 PIVX." });
-    else if (amount.greaterThan(Decimal(user.balance.toString()).mul(1e-8))) return Promise.reject({ message: "You do not have enough funds! Try tipping a smaller amount." });
+    else if (amount.lessThan(0.001)) return Promise.reject({ message: "The minimum amount allowed to tip is 0.001 PIVX." });
+    else if (amount.greaterThan(Decimal(user.balance.toString()).mul(1e-8))) return Promise.reject({ message: "You do not have sufficient funds!" });
 
     return Promise.resolve({});
 };
