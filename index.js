b450557f75ca396efc47a832cdec097c4b2be1b0
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-09-16 15:17:48 +0200

    Update to Manual Fetch

--- index.js
+++ index.js
@@ -5,60 +5,42 @@ global.srcRoot = path.resolve(__dirname);
 global.env = (process.argv[2] === '--production') ? process.env.NODE_ENV : "development";
 
 const Snoowrap = require('snoowrap');
-const snoostream = require('snoostream');
-let config;
 
-try {
-    config = require('./src/data/config.json');
-} catch (e) {
-    console.log('No configuration file found.');
-    console.log(e);
-    process.exit(0);
-}
+const dotenv = require('dotenv');
 
-const msgHandler = require('./src/handlers/handle_msg.js');
+dotenv.config({ path: './src/data/config.env' });
 
 const setupDatabase = require('./src/db/setup');
 
 const runPoll = require('./src/handlers/handle_DMs.js');
-
 const client = new Snoowrap({
-    userAgent   : config.auth.USER_AGENT,
-    clientId    : config.auth.CLIENT_ID,
-    clientSecret: config.auth.CLIENT_SECRET,
-    username    : config.auth.USERNAME,
-    password    : config.auth.PASSWORD
+    userAgent   : process.env.USER_AGENT,
+    clientId    : process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username    : process.env.USERNAME,
+    password    : process.env.PASSWORD
 });
 
-const text = `Hello there! I'm /u/pivxtipbot, the official PIVX Reddit Tip Bot! You have interacted with me for the first time, so here's some information about my functionalities and commands. Should any problem arise please contact my maker /u/Bueris.` +
+const text = `Hello there! I'm /u/pivxtipbot, the official PIVX Reddit Tip Bot! Since you have been interacted with me for the first time, I'm here to help you get to know the ropes.` +
 `\n\nTo begin using me, take a look at the following commands:` +
-`\n\n    !history - Your history of tips.\n\n    !transactions - Your transactions (deposits/withdrawals)\n\n    !balance - Check your account balance.\n\n    !deposit - Get a new one-time deposit address\n\n    !withdraw [amount] [address] - Withdraw funds from your account` +
-`\n\nIf you have existing balance, you can tip others by replying to a post/comment by them with \`!pivxtip [amount]\`. My code is fully open source! You can review it at http://tip.pivx.events . Have fun!`;
+`\n\n    !history - Your history of tips.\n\n    !transactions - Your transactions (deposit/withdraw)\n\n    !balance - Check your account balance.\n\n    !deposit - Get a new one-time deposit address\n\n    !withdraw [amount] [address] - Withdraw funds from your account` +
+`\n\nIf you have existing balance, you can tip others by replying to a post/comment by them using \`!pivxtip [amount]\`. You can get support and further detail over at /r/pivxtipbot . Have fun!`;
 
 global.welcomeMessage = async function (username) {
     return client.composeMessage({ to: username, subject: "Welcome to PIVX Tip Bot!", text });
 };
 
 global.toFixed = function (num, fixed) {
-    var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+    var re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?');
     return num.toString().match(re)[0];
 };
-
-const snooStream = snoostream(client);
-
-const commentStream = snooStream.commentStream('all', {rate: 2000});
-
-commentStream.on('post', (post) => {
-    msgHandler(post, client);
-});
 
 setupDatabase().then((result) => {
 
     global.agenda = result.agenda;
 
     console.log(`PIVX Tip Bot starting up...`);
+    runPoll(client);
 
     if (process.argv[2] !== '--no-daemon') fork('./src/worker');
 });
-
-runPoll(client);
