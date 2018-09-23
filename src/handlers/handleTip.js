b6584def79092b01f36878c28979e55d70915eef
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-09-23 23:37:29 +0200

    Update handleTip.js

--- src/handlers/handleTip.js
+++ src/handlers/handleTip.js
@@ -19,25 +19,25 @@ module.exports = async (original, comment, amount) => {
             await createUser(_tipper.name);
             rej('You didn\'t have an account, so I created one for you!');
         }
-        else if (!receiver) {
-            receiver = await createUser(_receiver.name);
-        }
         else if (receiver.username == tipper.username) {
             rej('You may not tip yourself!');
+        } else if (!receiver) {
+            receiver = await createUser(_receiver.name);
         }
-        else {
-            await User.tip(tipper, receiver, amount).then(() => {
 
-                const tip = new Tip({tipper: tipper._id, tipped: receiver._id, amount});
-                tip.save((err) => {
-                    if (err) rej(err);
-                    res(true);
-                });
 
-            }).catch((err) => {
-                rej(err.message);
+        await User.tip(tipper, receiver, amount).then(() => {
+
+            const tip = new Tip({tipper: tipper._id, tipped: receiver._id, amount});
+            tip.save((err) => {
+                if (err) rej(err);
+                res(true);
             });
-        }
+
+        }).catch((err) => {
+            rej(err.message);
+        });
+
     });
 
 };
