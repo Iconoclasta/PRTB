ae95a11b282bba60a26a8dc3b3e9516eec4f4ed8
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-09-24 02:19:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
 const path = require('path');
 const { fork } = require('child_process');
+
+const {User} = require('./src/db');
 
 global.srcRoot = path.resolve(__dirname);
 global.env = (process.argv[2] === '--production') ? process.env.NODE_ENV : "development";
@@ -42,5 +45,33 @@ setupDatabase().then((result) => {
     console.log(`PIVX Tip Bot starting up...`);
     runPoll(client);
 
-    if (process.argv[2] !== '--no-daemon') fork('./src/worker');
+    let worker;
+
+    if (process.argv[2] !== '--no-daemon') { worker = fork('./src/worker'); }
+
+    worker.on('message', async (data) => {
+        // TODO handle user ID and send message accordingly
+        if (data.deposit) await depositMessage(data);
+        else await withdrawMessage(data);
+    });
+
 });
+
+async function depositMessage (data) {
+
+    const user = await User.findById(data.userId);
+
+    if (!user) return;
+
+    return client.composeMessage({ to: user.username, subject: "Deposit Success", text: `Your deposit of **${data.amount}** PIVX has been credited to your account.`});
+}
+
+async function withdrawMessage (data) {
+
+    const user = await User.findById(data.userId);
+
+    if (!user) return;
+
+    if (!data.error && data.txid) return client.composeMessage({ to: user.username, subject: "Withdraw Success", text: `Your withdraw of **${data.amount}** PIVX has been executed. Your TXID is: ${data.txid}`});
+    else return client.composeMessage({ to: user.username, subject: "Withdraw Failed", text: `Your withdraw of **${data.amount}** PIVX has encountered an error. The error was: ${data.error}`});
+}
