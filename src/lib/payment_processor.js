62c04728d711f891c63d4affc9a3a77ac524cc90
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-09-24 03:42:05 +0200

    Update payment_processor.js

--- src/lib/payment_processor.js
+++ src/lib/payment_processor.js
@@ -1,174 +1,44 @@
-global.srcRoot = require('path').resolve('./');
-const {Transaction, User, Job} = require('../db');
+const {User, Tip} = require('../db');
 
-const PIVXClient = require('./pivx_client.js');
+module.exports = async (original, comment, amount) => {
+    return new Promise(async (res, rej) => {
+        const _tipper = await original.author;
+        const _receiver = await comment.author;
 
-class PaymentProcessor {
-    constructor (options) {
-        this.agenda = options.agenda;
-        this.pivxClient = options.pivxClient || new PIVXClient();
-        this.parent = options.parent || null;
-    }
 
-    reportException (e) {
-        console.error(e);
-    }
+        const tipper = await User.findOne({username: _tipper.name});
+        let receiver = await User.findOne({username: _receiver.name});
 
-    async performWithdraw (options) {
-        return this.withdraw(options).then(() => { return { success: true }; }).catch(err => { return {error: err}; });
-    }
-
-    async performDeposit (options) {
-        try {
-            await this.deposit(options);
-            return { success: true };
-        } catch (e) {
-            console.error(e);
-            return new Error(e);
+        async function createUser(username) {
+            await welcomeMessage(username);
+            return User.create({username});
         }
-    }
 
-    async getAddress () {
-        try {
-            return this.pivxClient.accountCreate();
-        } catch (e) {
-            this.reportException(e);
-            return { error: e };
+
+        if (!tipper) {
+            await createUser(_tipper.name);
+            rej('You didn\'t have an account, so I created one for you!');
         }
-    }
+        else if (!receiver) {
+            receiver = await createUser(_receiver.name);
+        }
+        else if (receiver.username == tipper.username) {
+            rej('You may not tip yourself!');
+        }
 
-    async checkDeposit () {
-        return this.pivxClient.listTransactions().then(txs => {
-            const promises = txs.map(tx => {
-                return new Promise((resolve, reject) => {
-                    if (tx.account === process.env.RPC_ACC) {
-                        Transaction.findOne({ txid: tx.txid }).then(async result => {
-                            if (!result) {
-                                await this.createDepositOrder(tx.txid, tx.address, tx.amount);
-                                resolve(true);
-                            }
-                        }).catch(reject);
-                    }
-                });
+
+        await User.tip(tipper, receiver, amount).then(() => {
+
+            const tip = new Tip({tipper: tipper._id, tipped: receiver._id, amount});
+            tip.save((err) => {
+                if (err) rej(err);
+                res(true);
             });
-            return Promise.all(promises);
+
         }).catch((err) => {
-            console.error('Daemon connection error: ' + err.stack);
-            return err;
+            rej(err.message);
         });
-    }
 
-    async createDepositOrder (txID, recipientAddress, amount) {
-        let job = await Job.findOne({ 'data.txid': txID });
+    });
 
-        if (!job) {
-            console.log('New transaction! TXID: ' + txID);
-
-            job = await this.agenda.create('deposit_order', { recipientAddress: recipientAddress, txid: txID, amount: amount });
-            return new Promise((resolve, reject) => {
-                job.save();
-                resolve();
-            });
-        }
-
-        return job;
-    }
-
-    /*
-        amount: {String}
-    */
-    async withdraw (job) {
-    // parameters
-        const userId = job.attrs.data.userId;
-        const recipientAddress = job.attrs.data.recipientAddress;
-        const amount = job.attrs.data.amount;
-
-        // Validate if user is present
-        let user = await User.findById(userId);
-        if (!user) throw new Error(`User ${userId} not found`);
-
-        // Step 1: Process transaction
-        let sendID;
-
-        // Step 2: Update user balance
-        if (!job.attrs.userStepCompleted) {
-            await new Promise((resolve, reject) => {
-                User.withdraw(user, amount).then(() => {
-                    Job.findByIdAndUpdate(job.attrs._id, { 'data.userStepCompleted': true }).then(() => {
-                        resolve(true);
-                    }).catch(err => {
-                        reject(err);
-                    });
-                }).catch(err => {
-                    reject(err);
-                });
-            }).catch(err => {
-                process.send({ id: user.id, amount, address: recipientAddress, txid: null, error: err.message });
-                throw new Error(err);
-            });
-        }
-
-        if (job.attrs.sendStepCompleted) {
-            sendID = job.attrs.txid;
-        } else {
-            const sent = /*new Promise(resolve => resolve('test'));*/ this.pivxClient.send(recipientAddress, amount);
-
-            await new Promise((resolve, reject) => {
-                sent.then((txid) => {
-                    resolve(txid);
-                }).catch(async err => {
-                    if (err) {
-                        await User.deposit(user, amount);
-                        process.send({ id: user.id, amount, address: recipientAddress, txid: null, error: err.message });
-                        reject(err);
-                    }
-                });
-            }).then(async (txid) => {
-                await Job.findOneAndUpdate({ _id: job.attrs._id }, { 'data.sendStepCompleted': true, 'data.txid': txid });
-                sendID = txid;
-            }).catch(err => {
-                throw new Error(err);
-            });
-        }
-
-        if (!job.attrs.transactionStepCompleted) {
-            // console.log(sendID);
-            await Transaction.create({ userId: userId, withdraw: amount, txid: sendID });
-            await Job.findByIdAndUpdate(job.attrs._id, { 'data.transactionStepCompleted': true });
-        }
-
-        // Step 3: Record Transaction
-
-        await new Promise(resolve => { process.send({ _id: user._id, amount, address: recipientAddress, txid: sendID }); resolve(); });
-
-        return sendID;
-    }
-
-    async deposit (job) {
-    // parameters
-        const txid = job.attrs.data.txid;
-        const recipientAddress = job.attrs.data.recipientAddress;
-        const amount = job.attrs.data.amount;
-
-        // Validate if user is present
-        let user = await User.findOne({ addr: recipientAddress });
-
-        if (!user) throw new Error(`User with address ${recipientAddress} not found`);
-
-        if (!job.attrs.userStepCompleted) {
-            await User.deposit(user, amount, txid);
-            await Job.findByIdAndUpdate(job.attrs._id, { 'data.userStepCompleted': true });
-        }
-
-        if (!job.attrs.transactionStepCompleted) {
-            await Transaction.create({ userId: user._id, deposit: amount, txid: txid });
-            await Job.findByIdAndUpdate(job.attrs._id, { 'data.transactionStepCompleted': true });
-        }
-
-        await new Promise(resolve => { process.send({ _id: user._id, amount, deposit: true }); resolve(); });
-
-        return txid;
-    }
-}
-
-module.exports = PaymentProcessor;
+};
