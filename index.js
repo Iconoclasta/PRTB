74a72587035042d8890fa893910dbb436d943662
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-10-07 19:13:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ dotenv.config({ path: './src/data/config.env' });
 const setupDatabase = require('./src/db/setup');
 
 const runPoll = require('./src/handlers/handle_DMs.js');
+if (!process.env.USER_AGENT || !process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.USERNAME || !process.env.PASSWORD) {
+    console.log('Credentials not found.');
+    process.exit(0);
+}
 const client = new Snoowrap({
     userAgent   : process.env.USER_AGENT,
     clientId    : process.env.CLIENT_ID,
