0a900ba8f1d7326f711ae8dad2c0bc7f65b3db66
Iconoclasta <40902868+Iconoclasta@users.noreply.github.com>
2018-10-07 19:27:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const setupDatabase = require('./src/db/setup');
 const runPoll = require('./src/handlers/handle_DMs.js');
 if (!process.env.USER_AGENT || !process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.USERNAME || !process.env.PASSWORD) {
     console.log('Credentials not found.');
+    console.log(process.env.USER_AGENT, process.env.CLIENT_ID, process.env.CLIENT_SECRET.length, process.env.USERNAME, process.env.PASSWORD.length)
     process.exit(0);
 }
 const client = new Snoowrap({
